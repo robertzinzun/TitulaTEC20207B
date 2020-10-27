0d57c41b137810c7bcc69e268abc16fe104c2e8e
robertzinzun <robertzinzun@gmail.com>
2020-10-27 13:47:16 -0600

    Update miScript.js
    calcular la calif maxima y minima

--- templates/JS/miScript.js
+++ templates/JS/miScript.js
@@ -6,7 +6,33 @@ var miFuncion=ocultarDivs;
 var alumno={
     "nombre":"Roberto Suárez Zinzún",
     "carrera":"ISC",
-    "calificaciones":[100,80,70,75,90]
+    "calificaciones":[
+        {
+            "nombre_materia":"FP",
+            "calificacion":100,
+            "semestre":1
+        },
+        {
+            "nombre_materia":"Mate 1",
+            "calificacion":80,
+            "semestre":1
+        },
+        {
+            "nombre_materia":"Quimica",
+            "calificacion":70,
+            "semestre":1
+        },
+        {
+            "nombre_materia":"POO",
+            "calificacion":75,
+            "semestre":2
+        },
+        {
+            "nombre_materia":"Mate 2",
+            "calificacion":90,
+            "semestre":2
+        }
+    ]
 };
 function mostrarDatosGenerales(){
     alert("Nombre:"+alumno.nombre+", carrera:"+alumno.carrera);
@@ -19,8 +45,14 @@ function mostrarCalificaciones(){
 function promedioCalificaciones(){
     var promedio=0;
     for(i=0;i<alumno.calificaciones.length;i++){
-        promedio+=alumno.calificaciones[i];
+        promedio+=alumno.calificaciones[i].calificacion;
     }
     promedio=promedio/alumno.calificaciones.length
     alert("Promedio "+promedio);
+}
+function califMaxima(){
+
+}
+function califMinima(){
+
 }
