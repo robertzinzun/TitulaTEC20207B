78363d890e42a5412d79323c9964f85401d84180
MarcoChavezFdez <mchavez297@accitesz.com>
2020-10-27 13:38:48 -0600

    Funcion promedio

--- templates/JS/miScript.js
+++ templates/JS/miScript.js
@@ -14,5 +14,13 @@ function mostrarDatosGenerales(){
 function mostrarCalificaciones(){
     for(i=0;i<alumno.calificaciones.length;i++){
         alert("Calificación "+(i+1)+":"+alumno.calificaciones[i]);
+    } 
+}
+function promedioCalificaciones(){
+    var promedio=0;
+    for(i=0;i<alumno.calificaciones.length;i++){
+        promedio+=alumno.calificaciones[i];
     }
+    promedio=promedio/alumno.calificaciones.length
+    alert("Promedio "+promedio);
 }
